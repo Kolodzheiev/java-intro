2904731da9e0a21a90f5ffc73ce7b46d4e1fe544
Kolodzheiev <kolodgeev.sergey@gmail.com>
2022-04-26 23:45:09 +0100

    Add homework1

---
+++ src/com/java/classwork/Main.java
@@ -0,0 +1,12 @@
+package com.java.classwork;
+
+public class Main {
+     //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello world");
+
+
+
+    }
+}
---
+++ src/com/java/homework/VariablesSubstitution.java
@@ -0,0 +1,19 @@
+package com.java.homework;
+
+import java.util.Scanner;
+
+public class VariablesSubstitution {
+    public static void main(String[] args) {
+        float a, b, temp;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введіть число a");
+        a = in.nextFloat();
+        System.out.println("Введіть число b");
+        b = in.nextFloat();
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a=" + a + "b=" + b);
+
+    }
+}
