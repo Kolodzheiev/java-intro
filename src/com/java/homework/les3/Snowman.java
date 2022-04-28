da56013b87e2b680e861e50c0454e5426fc64d34
Kolodzheiev <kolodgeev.sergey@gmail.com>
2022-04-28 23:08:42 +0100

    Add homework1

--- src/com/java/homework/les3/Snowman.java
+++ src/com/java/homework/les3/Snowman.java
@@ -9,12 +9,12 @@ public class Snowman {
         double radius1 = 1;
         double radius2 = 0.5;
         double radius3 = 0.2;
-        double smash1 = (double) 4 / 3 * Math.PI * Math.pow(radius1, 3);
-        double smash2 = (double) 4 / 3 * Math.PI * Math.pow(radius2, 3);
-        double smash3 = (double) 4 / 3 * Math.PI * Math.pow(radius3, 3);
-        double weight1 = smash1 * density;
-        double weight2 = smash2 * density;
-        double weight3 = smash3 * density;
+        double volume1 = (double) 4 / 3 * Math.PI * Math.pow(radius1, 3);
+        double volume2 = (double) 4 / 3 * Math.PI * Math.pow(radius2, 3);
+        double volume3 = (double) 4 / 3 * Math.PI * Math.pow(radius3, 3);
+        double weight1 = volume1 * density;
+        double weight2 = volume2 * density;
+        double weight3 = volume3 * density;
         double snowman = weight1 + weight2 + weight3;
         System.out.println("Сніговик важить " + snowman);
 
