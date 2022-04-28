2cee3efb01c93a07dcb47ecb60cbe9b6180006c3
Kolodzheiev <kolodgeev.sergey@gmail.com>
2022-04-28 23:06:50 +0100

    Add homework1

--- src/com/java/homework/les3/MathTest.java
+++ src/com/java/homework/les3/MathTest.java
@@ -1,24 +1,28 @@
 package com.java.homework.les3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class MathTest {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Введіть чисо a");
-        int a = in.nextInt();
+        int a = Integer.parseInt(in.readLine());
         System.out.println("Введіть чисо b");
-        int b = in.nextInt();
+        int b = Integer.parseInt(in.readLine());
         System.out.println("Введіть чисо c");
-        int c = in.nextInt();
+        int c = Integer.parseInt(in.readLine());
         double result = (java.lang.Math.abs(a - b) / java.lang.Math.pow((a + b), 3) - java.lang.Math.sqrt(c));
         System.out.println("|a-b|/(a+b)^3-√с = " + result);
 
 
     }
 
-    }
+}
 
 
 
