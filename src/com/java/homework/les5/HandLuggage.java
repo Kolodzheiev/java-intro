7415676ce1c13cdac71b9160e5650705f6ec259e
serhii <kolodgeev.sergey@gmail.com>
2022-05-06 19:57:54 +0100

    classwork

--- src/com/java/homework/les5/HandLuggage.java
+++ src/com/java/homework/les5/HandLuggage.java
@@ -17,11 +17,19 @@ public class HandLuggage {
         System.out.println("Введіть ширину валізи");
         int width = Integer.parseInt(in.readLine());
 
-        if (length <= 55 && height <= 40 && width <= 20){
-            System.out.println("Багаж проходить");
+        boolean suitcase = checkingSuitcases(length, height, width);
 
-        } else if (length <= 40 && height <= 53 && width <= 22) {
-            System.out.println("Багаж проходить");
-        }else System.out.println("Бфгфж не проходить");
+        String message = suitcase ? "Ручна ккладь" : "Не ручна кладь";
+        System.out.println(message);
+
+
+    }
+
+    public static boolean checkingSuitcases(int length, int height, int width) {
+        if (length <= 55 && height <= 40 && width <= 20) {
+            return true;
+
+        } else return length <= 40 && height <= 53 && width <= 22;
     }
 }
+
