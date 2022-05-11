380c5e74dca405e6585ac6c17ab73176051e7631
serhii <kolodgeev.sergey@gmail.com>
2022-05-11 17:32:31 +0100

    classwork

--- src/com/java/homework/les7/Japan.java
+++ src/com/java/homework/les7/Japan.java
@@ -3,7 +3,7 @@ package com.java.homework.les7;
 public class Japan {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
-            if (i == 4|| i == 9|| i%10==4|| i%10==9||i/10==4||i/10==9)
+            if (i == 4 || i == 9 || i % 10 == 4 || i % 10 == 9 || i / 10 == 4 || i / 10 == 9)
                 continue;
             System.out.println(i);
         }
