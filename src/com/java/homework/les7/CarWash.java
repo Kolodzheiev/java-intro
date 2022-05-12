d02cbb0509323ceed564372daacbad56cbdcab1a
serhii <kolodgeev.sergey@gmail.com>
2022-05-12 13:36:05 +0100

    classwork

--- src/com/java/homework/les7/CarWash.java
+++ src/com/java/homework/les7/CarWash.java
@@ -10,22 +10,24 @@ public class CarWash {
         System.out.println("How many cars need to be washed");
         int dirtyCars = Integer.parseInt(in.readLine());
 
-    //    washingSteps1(dirtyCars);
-        washingSteps2(dirtyCars);
+        // washingOptions1(dirtyCars);
+        washingOptions2(dirtyCars);
 
         System.out.println("Cars washed");
 
     }
-    public static void washingSteps1(int dirtyCars){
-        while (dirtyCars != 0) {
 
+    public static void washingOptions1(int dirtyCars) {
+        while (dirtyCars != 0) {
             soapCar(dirtyCars);
             washCar(dirtyCars);
             dryCar(dirtyCars);
             dirtyCars--;
         }
-    }public static void washingSteps2(int dirtyCars){
-        for (; dirtyCars!=0;dirtyCars--){
+    }
+
+    public static void washingOptions2(int dirtyCars) {
+        for (; dirtyCars != 0; dirtyCars--) {
             soapCar(dirtyCars);
             washCar(dirtyCars);
             dryCar(dirtyCars);
