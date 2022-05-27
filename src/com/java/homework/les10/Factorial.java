5c370024ecd2bb3c0c8f593c0413b4610b133276
serhii <kolodgeev.sergey@gmail.com>
2022-05-27 17:41:34 +0100

    classwork

--- src/com/java/homework/les10/Factorial.java
+++ src/com/java/homework/les10/Factorial.java
@@ -4,15 +4,20 @@ import com.java.common.ConsoleUtils;
 
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println("Введите число ");
+        System.out.println("Введіть число для визначеня факторіала ");
         int N = ConsoleUtils.getNumber();
         System.out.println(factorial(N));
 
     }
+
     public static int factorial(int number) {
-        if(number==1){
+        if (number == 1) {
             return number;
         }
-        return number*factorial(number-1);
+        if (number < 0) {
+            System.out.println("Не треба тобі факторіал відьємного числа");
+            return -1;
+        }
+        return number * factorial(number - 1);
     }
 }
