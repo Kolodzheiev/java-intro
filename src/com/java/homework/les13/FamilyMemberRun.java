0214abd902119c42e62e7a1c879594aa500e65ad
serhii <kolodgeev.sergey@gmail.com>
2022-06-07 16:22:44 +0100

    classwork

--- src/com/java/homework/les13/FamilyMemberRun.java
+++ src/com/java/homework/les13/FamilyMemberRun.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 
 public class FamilyMemberRun {
     public static void main(String[] args) throws IOException {
-        System.out.println("Мама есть? Якшо так, то як її звати? ");
+        System.out.println("Мама є? Якшо так, то як її звати? ");
         String mother = ConsoleUtils.READER.readLine();
         FamilyMember familyMemberMother = null;
         if(mother != null && !mother.isBlank()) {
             familyMemberMother = new FamilyMember(mother);
         }
-        System.out.println("Папа есть? Якшо так, то як його звати? ");
+        System.out.println("Папа є? Якшо так, то як його звати? ");
         String father = ConsoleUtils.READER.readLine();
         FamilyMember familyMemberFather = null;
         if(father != null && !father.isBlank()) {
