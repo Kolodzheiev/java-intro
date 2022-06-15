35ebd662de69f78f3104d3d980ebd16ffda3e723
serhii <kolodgeev.sergey@gmail.com>
2022-06-15 21:13:00 +0100

    classwork

--- src/com/java/homework/alg/Shift.java
+++ src/com/java/homework/alg/Shift.java
@@ -10,30 +10,36 @@ public class Shift {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(Arrays.toString(array));
         int n = Integer.parseInt(ConsoleUtils.READER.readLine());
-        /*for (int i = 0; i < n; i++) {
+        //shiftArray(array, n);
+        //Зсув в ліво
+        shiftArrayLeft(array, n);
+        //Зсув в право
+        //shiftArrayRight(array, n);
+    }
+
+    private static void shiftArray(int[] array, int n) {
+        for (int i = 0; i < n; i++) {
             int tmp = array[0];
-            for (int j = 0; j < array.length - 1; j++)
-                array[j] = array[j + 1];
+            System.arraycopy(array, 1, array, 0, array.length - 1);
             array[array.length - 1] = tmp;
         }
-        System.out.println(Arrays.toString(array));*/
+        System.out.println(Arrays.toString(array));
+    }
 
-//      Зсув в ліво
-        /*int length = array.length;
+    private static void shiftArrayLeft(int[] array, int n) {
+        int length = array.length;
         int[] arrayShift = new int[length];
         System.arraycopy(array, n, arrayShift, 0, length - n);
         System.arraycopy(array, 0, arrayShift, length - n, n);
-        System.out.println(Arrays.toString(arrayShift));*/
+        System.out.println(Arrays.toString(arrayShift));
+    }
 
-//      Зсув в право
+    private static void shiftArrayRight(int[] array, int n) {
         int length = array.length;
         int[] arrayShift = new int[length];
-        System.arraycopy(array, 0, arrayShift, length - n,  n);
+        System.arraycopy(array, 0, arrayShift, length - n, n);
         System.arraycopy(array, n, arrayShift, 0, length - n);
-
         System.out.println(Arrays.toString(arrayShift));
-
-
     }
 
 }
---
+++ src/com/java/homework/alg/Sum.java
@@ -0,0 +1,25 @@
+package com.java.homework.alg;
+
+import com.java.common.ConsoleUtils;
+
+import java.io.IOException;
+
+public class Sum {
+    public static void main(String[] args) throws IOException {
+        int[] arr = {1, 4, 45, 6, 10, 8};
+        System.out.println("Введіть сумму");
+        int sum = Integer.parseInt(ConsoleUtils.READER.readLine());
+
+        for (int i = 0; i < arr.length; ++i) {
+            int rest = sum - arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] == rest) {
+                    System.out.println("Сума " + sum + " це (" + arr[i] + ", " + arr[j] + ")");
+                }
+            }
+        }
+        System.out.println("Немає такої суми");
+    }
+
+}
+
